3c7cf2df5a9773755c73f2f24e5790a6de45df01
Komron <knmv2311@gmail.com>
2024-07-03 11:17:01 +0500

    first commit

---
+++ primer/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let myArray = [100, "London", true];
+myArray[0] = "Tuesday";
+let val = myArray[0];
+console.log(`Value: ${val}`);
---
+++ primer/index.ts
@@ -0,0 +1,6 @@
+let myArray: (number | string | boolean)[] = [100, "London", true];
+
+myArray[0] = "Tuesday";
+
+let val = myArray[0];
+console.log(`Value: ${val}`);
