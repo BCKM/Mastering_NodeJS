5b914d1fbbff5722588069707974584e2b59bb3d
Komron <knmv2311@gmail.com>
2024-07-23 15:53:59 +0500

    mch

--- primer/index.js
+++ primer/index.js
@@ -1,6 +1,31 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let myArray = [100, "London", true];
-myArray[0] = "Tuesday";
-let val = myArray[0];
-console.log(`Value: ${val}`);
+class Product {
+    constructor(name, price, category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+    name;
+    price;
+    category;
+    printDetails() {
+        if (this.category != undefined) {
+            console.log(`Name: ${this.name}, Price: ${this.price}, ` + `Category: ${this.category}`);
+        }
+        else {
+            console.log(`Name: ${this.name}, Price: ${this.price}`);
+        }
+    }
+}
+let hat = new Product("Hat", 100);
+let boots = new Product("Boots", 100, "Snow gear");
+// function printDetails(product : { name: string, price: number, category?: string}) {
+//     if (product.category != undefined) {
+//         console.log(`Name: ${product.name}, Price: ${product.price}, ` + `Category: ${product.category}`);
+//     } else {
+//         console.log(`Name: ${product.name}, Price: ${product.price}`);
+//     }
+// }
+hat.printDetails();
+boots.printDetails();
--- primer/index.ts
+++ primer/index.ts
@@ -1,6 +1,35 @@
-let myArray: (number | string | boolean)[] = [100, "London", true];
+class Product {
 
-myArray[0] = "Tuesday";
+    constructor(name: string, price: number, category?: string) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
 
-let val = myArray[0];
-console.log(`Value: ${val}`);
+    name: string;
+    price: number;
+    category?: string;
+
+    printDetails() {
+        if (this.category != undefined) {
+            console.log(`Name: ${this.name}, Price: ${this.price}, ` + `Category: ${this.category}`);
+        } else {
+            console.log(`Name: ${this.name}, Price: ${this.price}`);
+        }
+    }
+}
+
+let hat = new Product("Hat", 100);
+
+let boots = new Product("Boots", 100, "Snow gear");
+
+// function printDetails(product : { name: string, price: number, category?: string}) {
+//     if (product.category != undefined) {
+//         console.log(`Name: ${product.name}, Price: ${product.price}, ` + `Category: ${product.category}`);
+//     } else {
+//         console.log(`Name: ${product.name}, Price: ${product.price}`);
+//     }
+// }
+
+hat.printDetails();
+boots.printDetails();
